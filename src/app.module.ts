34f0119ba419df9755f61f94b9b8cdb790097a77
zhouwy <18842622631@126.com>
2023-12-08 17:35:29 +0800

    init

---
+++ src/app.module.ts
@@ -0,0 +1,18 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import configuration from './config/configuration';
+
+@Module({
+    imports: [
+        ConfigModule.forRoot({
+            isGlobal: true,
+            envFilePath: `.env.${process.env.NODE_ENV}`,
+            load: [configuration]
+        })
+    ],
+    controllers: [AppController],
+    providers: [AppService]
+})
+export class AppModule {}
